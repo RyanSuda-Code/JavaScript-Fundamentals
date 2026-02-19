35b5ed078eac61057dd5bca3a127409c2461fe4d
RyanSuda-Code <sudaryan49@yahoo.com>
2026-02-19 21:53:32 +0900

    Created a leapYearCounter exercise that focuses on functions, returns, and conditionals.

---
+++ Functions/leapYearCalculator.js
@@ -0,0 +1,19 @@
+let year;
+
+const isLeapYear = (num) => {
+    if (num % 400 === 0) {
+        return `${num} is a leap year.`;
+    } else if (num % 100 === 0) {
+        return `${num} is not a leap year.`;
+    } else if (num % 4 === 0) {
+        return `${num} is a leap year.`;
+    } else {
+        return `${num} is not a leap year.`;
+    }
+}
+
+year = 2024;
+let result = isLeapYear(year);
+console.log(result);
+year = 1900;
+console.log(result);
