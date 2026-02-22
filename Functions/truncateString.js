da73c458c0e2200128c7465e9667a5f86cfaaba3
RyanSuda-Code <sudaryan49@yahoo.com>
2026-02-22 14:13:11 +0900

    practiced string length, conditional logic, slice(), and template literals.

---
+++ Functions/truncateString.js
@@ -0,0 +1,10 @@
+const truncateString = (str, num) => {
+    if (str.length > num) {
+        return `${str.slice(0, num)}...`
+    } else {
+        return str;
+    }
+}
+
+
+console.log(truncateString("Peter Piper picked a peck of pickled peppers", 11));
